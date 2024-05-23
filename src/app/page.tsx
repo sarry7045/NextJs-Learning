a0653b7793065080ea255785c40718319abece6c
Suraj Yadav <sarry7045@gmail.com>
2024-05-23 20:00:27 +0530

    Data Fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  
+  const apiResponse = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await apiResponse.json();
+  console.log("Userss", users);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,14 +9,14 @@ export const config = {
   matcher: '/about/:path*',
 }
 
-export function middleware(request) {
-    if (request.nextUrl.pathname.startsWith('/about')) {
-      return NextResponse.rewrite(new URL('/about-2', request.url))
-    }
+// export function middleware(request) {
+//     if (request.nextUrl.pathname.startsWith('/about')) {
+//       return NextResponse.rewrite(new URL('/about-2', request.url))
+//     }
    
-    if (request.nextUrl.pathname.startsWith('/dashboard')) {
-      return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-    }
-  }
+//     if (request.nextUrl.pathname.startsWith('/dashboard')) {
+//       return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+//     }
+//   }
 
  
